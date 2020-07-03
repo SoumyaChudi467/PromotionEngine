2f3e6fae7c56022fadf6e9548fc341df79546147
soumya.chudi <soumya.chudi@cgi.com>
2020-07-03 10:18:59 +0530

    Add project files.

---
+++ PromotionEngine.Handlers.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PromotionEngine.Handlers.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
